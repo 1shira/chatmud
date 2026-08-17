bad1b604d9808a5ca77a2ad49c34e659b678b91f
shira <98327283+1shira@users.noreply.github.com>
2026-08-17 11:29:14 +0200

    db

--- api/model.ts
+++ api/model.ts
@@ -1,9 +1,17 @@
 import dotenv from 'dotenv';
 import pg from 'pg';
 
-dotenv.config({ path: '.' });
+dotenv.config({ path: '..' });
 
-const pool = new pg.Pool();
+const { PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE } = process.env;
+
+const pool = new pg.Pool({
+  host: PGHOST,
+  port: Number(PGPORT),
+  user: PGUSER,
+  password: PGPASSWORD,
+  database: PGDATABASE,
+});
 const query = (text: string, params: string[]) => pool.query(text, params);
 
 export {};
